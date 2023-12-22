b4dbe27e99fbcb101a52f0a3a9ff4168bbea7b62
Abdullah-Bataineh <78338087+Abdullah-Bataineh@users.noreply.github.com>
2023-12-22 18:26:34 +0300

    create project

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,351 @@
+﻿using System.Drawing;
+
+namespace WindowsFormsApp1
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.label7 = new System.Windows.Forms.Label();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.label6 = new System.Windows.Forms.Label();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.label8 = new System.Windows.Forms.Label();
+			this.textBox6 = new System.Windows.Forms.TextBox();
+			this.panel3 = new System.Windows.Forms.Panel();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label11 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label10 = new System.Windows.Forms.Label();
+			this.label9 = new System.Windows.Forms.Label();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.panel1.SuspendLayout();
+			this.panel2.SuspendLayout();
+			this.panel3.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.BackColor = System.Drawing.Color.LightGray;
+			this.textBox1.Font = new System.Drawing.Font("Monotype Koufi", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox1.Location = new System.Drawing.Point(122, 72);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(382, 95);
+			this.textBox1.TabIndex = 0;
+			this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
+			this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.Cyan;
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Location = new System.Drawing.Point(339, 310);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(163, 40);
+			this.button1.TabIndex = 5;
+			this.button1.Text = "DeCrypted";
+			this.button1.UseVisualStyleBackColor = false;
+			// 
+			// button2
+			// 
+			this.button2.BackColor = System.Drawing.Color.RosyBrown;
+			this.button2.FlatAppearance.BorderColor = System.Drawing.Color.RosyBrown;
+			this.button2.FlatAppearance.BorderSize = 0;
+			this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button2.Location = new System.Drawing.Point(122, 310);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(163, 40);
+			this.button2.TabIndex = 6;
+			this.button2.Text = "Crypted";
+			this.button2.UseVisualStyleBackColor = false;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Font = new System.Drawing.Font("Monotype Koufi", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox2.Location = new System.Drawing.Point(120, 363);
+			this.textBox2.Multiline = true;
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(382, 95);
+			this.textBox2.TabIndex = 7;
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.Silver;
+			this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.panel1.Controls.Add(this.label7);
+			this.panel1.Controls.Add(this.textBox5);
+			this.panel1.Controls.Add(this.label6);
+			this.panel1.Controls.Add(this.textBox4);
+			this.panel1.Controls.Add(this.label5);
+			this.panel1.Controls.Add(this.textBox3);
+			this.panel1.Location = new System.Drawing.Point(122, 182);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(184, 120);
+			this.panel1.TabIndex = 9;
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label7.Location = new System.Drawing.Point(4, 84);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(46, 17);
+			this.label7.TabIndex = 5;
+			this.label7.Text = "Offset";
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(56, 83);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(100, 20);
+			this.textBox5.TabIndex = 4;
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label6.Location = new System.Drawing.Point(4, 49);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(46, 17);
+			this.label6.TabIndex = 3;
+			this.label6.Text = "Offset";
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(56, 48);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(100, 20);
+			this.textBox4.TabIndex = 2;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.Location = new System.Drawing.Point(4, 11);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(46, 17);
+			this.label5.TabIndex = 1;
+			this.label5.Text = "Offset";
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(56, 10);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(100, 20);
+			this.textBox3.TabIndex = 0;
+			// 
+			// panel2
+			// 
+			this.panel2.BackColor = System.Drawing.Color.Silver;
+			this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.panel2.Controls.Add(this.label8);
+			this.panel2.Controls.Add(this.textBox6);
+			this.panel2.Location = new System.Drawing.Point(312, 207);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(192, 95);
+			this.panel2.TabIndex = 10;
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label8.Location = new System.Drawing.Point(4, 5);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(46, 17);
+			this.label8.TabIndex = 12;
+			this.label8.Text = "Offset";
+			// 
+			// textBox6
+			// 
+			this.textBox6.Location = new System.Drawing.Point(56, 4);
+			this.textBox6.Name = "textBox6";
+			this.textBox6.Size = new System.Drawing.Size(100, 20);
+			this.textBox6.TabIndex = 11;
+			// 
+			// panel3
+			// 
+			this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(15)))), ((int)(((byte)(31)))));
+			this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.panel3.Controls.Add(this.label4);
+			this.panel3.Controls.Add(this.label2);
+			this.panel3.Controls.Add(this.label11);
+			this.panel3.Controls.Add(this.label1);
+			this.panel3.Controls.Add(this.label10);
+			this.panel3.Controls.Add(this.label9);
+			this.panel3.Location = new System.Drawing.Point(1, 0);
+			this.panel3.Name = "panel3";
+			this.panel3.Size = new System.Drawing.Size(513, 513);
+			this.panel3.TabIndex = 11;
+			this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.ForeColor = System.Drawing.SystemColors.ControlLight;
+			this.label4.Location = new System.Drawing.Point(308, 179);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(119, 17);
+			this.label4.TabIndex = 17;
+			this.label4.Text = "Vigenere Option :";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.ForeColor = System.Drawing.SystemColors.ControlDark;
+			this.label2.Location = new System.Drawing.Point(2, 362);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(72, 24);
+			this.label2.TabIndex = 16;
+			this.label2.Text = "Result :";
+			// 
+			// label11
+			// 
+			this.label11.AutoSize = true;
+			this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label11.ForeColor = System.Drawing.SystemColors.ControlDark;
+			this.label11.Location = new System.Drawing.Point(2, 186);
+			this.label11.Name = "label11";
+			this.label11.Size = new System.Drawing.Size(107, 17);
+			this.label11.TabIndex = 15;
+			this.label11.Text = "Caesar Option :";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ForeColor = System.Drawing.SystemColors.ControlDark;
+			this.label1.Location = new System.Drawing.Point(2, 75);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(57, 24);
+			this.label1.TabIndex = 14;
+			this.label1.Text = "Text :";
+			// 
+			// label10
+			// 
+			this.label10.AutoSize = true;
+			this.label10.BackColor = System.Drawing.SystemColors.ActiveBorder;
+			this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label10.ForeColor = System.Drawing.SystemColors.Highlight;
+			this.label10.Location = new System.Drawing.Point(126, 29);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(0, 20);
+			this.label10.TabIndex = 13;
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label9.ForeColor = System.Drawing.SystemColors.ControlDark;
+			this.label9.Location = new System.Drawing.Point(2, 23);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(60, 24);
+			this.label9.TabIndex = 12;
+			this.label9.Text = "Status";
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+			this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+			this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+			this.pictureBox1.Location = new System.Drawing.Point(510, 0);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(485, 513);
+			this.pictureBox1.TabIndex = 12;
+			this.pictureBox1.TabStop = false;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.ClientSize = new System.Drawing.Size(993, 514);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.panel3);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.panel2.ResumeLayout(false);
+			this.panel2.PerformLayout();
+			this.panel3.ResumeLayout(false);
+			this.panel3.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.TextBox textBox6;
+		private System.Windows.Forms.Panel panel3;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label11;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label4;
+	}
+}
+
